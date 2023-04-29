be19453af4880406ae591eb2aba4c8126b5da8dd
brock810 <130688727+brock810@users.noreply.github.com>
2023-04-29 11:49:51 -0500

    another file

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stripe.Treasury;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+        }
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+
+    }
+}
+    
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace BankEncapsulation
+﻿using Stripe.Treasury;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var brocksAccount = new BankAccount();
+
+            Console.WriteLine($"How much are u willing to deposit?");
+
+            var AmountToDeposit = double.Parse( Console.ReadLine() );
+
+            brocksAccount.Deposit(AmountToDeposit);
+
+            Console.WriteLine($"Thank you your balance is now {brocksAccount.GetBalance()}");
         }
     }
 }
